caf9e778f74e348de80e5e4f5591fe50c0c4be56
malinakupina <nmalina@gmail.com>
2024-12-21 13:03:10 +0100

    Update app.js nocookies sessio at all

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import session from 'express-session';
-import session from 'cookie-session';
 import path from 'path';
 import multer from 'multer';
 import { fileURLToPath } from 'url';
