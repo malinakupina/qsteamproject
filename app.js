85d4770f3586cf6cc6318aa3e95bf525c9743cca
malinakupina <nmalina@gmail.com>
2024-12-24 18:29:48 +0100

    Update app.js rcss

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
-const session = require('express-session');
+
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
