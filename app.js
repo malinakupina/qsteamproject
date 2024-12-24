c005e709c51f4e79dc18f495ad59629e7877793c
malinakupina <nmalina@gmail.com>
2024-12-24 18:40:37 +0100

    Update app.js import from model

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { ObjectId } from 'mongodb'; // Uključi ovo ako koristiš MongoDB bez Mongoose
-import Project from './model';  // Import Project model
+import Project from './model/Project';  // Import Project model
 
 // Učitavanje vrednosti iz .env fajla
 dotenv.config();
