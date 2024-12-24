d5e1a25b00b4837842d0a05653fa49bf9d349c1b
malinakupina <nmalina@gmail.com>
2024-12-24 18:36:52 +0100

    Update Project.js i tako to

--- model/Project.js
+++ model/Project.js
@@ -1,1 +1,26 @@
+import mongoose from 'mongoose';
+
+// Definisanje schema za projekat
+const projectSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    options: [
+        {
+            name: { type: String, required: true },
+            posts: [
+                {
+                    title: String,
+                    content: String,
+                    imageUrl: String,
+                    date: { type: Date, default: Date.now }
+                }
+            ]
+        }
+    ]
+});
+
+// Kreiranje modela na osnovu schema
+const Project = mongoose.model('Project', projectSchema);
+
+export default Project;
 
